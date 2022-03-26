edf8dcebe1c13eb54a596fe1c69d84d5e90920bf
abner-ferreira <rm93576@fiap.com.br>
2022-03-26 13:08:10 -0300

    Adicionados os exercicios de 16 a 20

---
+++ ex16.java
@@ -0,0 +1,25 @@
+package Exercicios;
+import java.util.Scanner;
+public class ex16 {
+
+	public static void main(String[] args) {
+		Scanner ler = new Scanner (System.in);
+		double cateto1, cateto2, hipotenusa;
+		
+		System.out.println("Digite o primeiro valor: ");
+		cateto1 = ler.nextDouble();
+		System.out.println("Digite o segundo valor: ");
+		cateto2 = ler.nextDouble();
+		System.out.println("Digite o terceiro valor: ");
+		hipotenusa = ler.nextDouble();
+		
+		if ( hipotenusa * hipotenusa == cateto1*cateto1 + cateto2*cateto2) {
+			System.out.println("Esse triângulo pode ser um triângulo retângulo!");
+		}else {
+			System.out.println("Esse triângulo não pode ser um triângulo retângulo!");
+		}
+		
+		
+	}
+
+}
---
+++ ex18.java
@@ -0,0 +1,40 @@
+package Exercicios;
+import java.util.Scanner;
+public class ex18 {
+
+	public static void main(String[] args) {
+		Scanner ler = new Scanner (System.in);
+		double aceleracao,vinicial,tempo,velocidade;
+		System.out.printf("Qual é a aceleração do carro em m/s? ");
+		aceleracao = ler.nextDouble();
+		System.out.printf("Qual é a velocidade inicial do carro em m/s? ");
+		vinicial = ler.nextDouble();
+		System.out.printf("Qual foi o tempo do percurso do carro em segundos? ");
+		tempo = ler.nextDouble();
+		velocidade = vinicial + (aceleracao*tempo);
+		velocidade = velocidade * 3.6 ;
+		if (velocidade == 0 ) {
+			System.out.println("Seu veiculo está parado.");
+			
+		}else {
+			if(velocidade <= 40) {
+				System.out.println("Seu veiculo está muito lento.");
+			} else {
+				if(velocidade > 40 && velocidade <= 60 ) {
+					System.out.println("Seu veiculo está na velocidade permitida.");
+				}else {
+					if(velocidade > 60 && velocidade <= 80) {
+						System.out.println("Seu veiculo está em velocidade de cruzeiro.");
+					} else if (velocidade > 80  && velocidade <= 120) {
+						System.out.println("Seu veiculo está rápido.");
+					} else {
+						System.out.println("Seu veiculo está muito rápido.");
+					}
+				}
+			}
+		}
+		
+		
+	}
+
+}
